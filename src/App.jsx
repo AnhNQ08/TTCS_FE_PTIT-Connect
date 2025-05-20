a89d45875b3705296b944857a8cb119fda8712b6
Quang Anh <nguyenquanganh10082004@gmail.com>
2025-05-20 20:42:24 +0700

    fix all file khong ban duy anh mang

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Home";
-import LoginPage from "./pages/Login";
-import RegisterPage from "./pages/Register";
-import FriendPage from "./pages/Friend";
-import MessengerPage from "./pages/Messenger";
-import NotificationPage from "./pages/Notification";
-import MyProfilePage from "./pages/MyProfile";
-import AddPostPage from "./pages/AddPostPage";
-import OtherUserProfile from "./pages/OtherUserProfile";
+import { Routes, Route } from "react-router-dom";
+import { AppRouters } from "./routes";
 
-function App() {
+const MyComponent = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/friends" element={<FriendPage />} />
-        <Route path="/messenger" element={<MessengerPage />} />
-        <Route path="/notification" element={<NotificationPage />} />
-        <Route path="/profile" element={<MyProfilePage />} />
-        <Route path="/add-post" element={<AddPostPage />} />
-        <Route path="/other-user-profile" element={<OtherUserProfile />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/*" element={<AppRouters />} />
+    </Routes>
   );
-}
+};
 
-export default App;
+export default MyComponent;
